fbbcaac98b76d2028a15fc6448600c662f158ca0
Aliaa osama <aliaaosama97@gmail.com>
2024-12-15 15:08:14 +0200

    some font updates

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Logo from '../../assets/logo.png'
 import cart from '../../assets/cart.png'
-import {Bebas_Neue} from '@next/font/google'
+import {Bebas_Neue, Oswald, Raleway} from 'next/font/google'
 
 const bebas = Bebas_Neue({
   weight: '400', // Optional, choose weight
+  subsets: ['latin'], // Optional, choose subset
+});
+const oswald = Oswald({
+  weight: '600', // Optional, choose weight
   subsets: ['latin'], // Optional, choose subset
 });
 export default function Header() {
@@ -18,13 +22,13 @@ export default function Header() {
             alt="logo"
             className="relative z-10 w-8 h-8 mr-2"
           />
-            <h1 className={`${bebas.className}text-lg font-bold text-gray-800 uppercase tracking-wide`}>
+            <h1 className={`${bebas.className} text-lg font-extrabold text-black uppercase tracking-wide`}>
               FITNESS CLUB
             </h1>
           </div>
-        <div className="flex gap-6 text-base font-bold text-gray-800 uppercase tracking-wide">
-          <a href="/" className="hover:text-red-700">Join <br/>Team</a>
-          <a href="/" className="hover:text-red-700">Free<br/> Pass</a>
+        <div className={`flex gap-10 text-base text-black uppercase font-semibold`}>
+          <a href="/" className={`${oswald.className} hover:text-red-700`}>Join<br/> Team</a>
+          <a href="/" className={`${oswald.className} hover:text-red-700`}>Free<br/> Pass</a>
           <a href="/">
              {/* logo Image */}
             <img
